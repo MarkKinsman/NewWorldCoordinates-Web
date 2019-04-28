67769f1805730d3d74755f3a307441566cf178e2
Petr Mitev <p.mitevpi@gmail.com>
2019-04-27 18:40:27 -0700

    tooltip cleanup

--- src/components/FloorPlan.vue
+++ src/components/FloorPlan.vue
@@ -83,7 +83,7 @@ export default {
             .html(
               d.summary +
                 "<br/>" +
-                d.summary +
+                d.comments +
                 "<br/>" +
                 d.note +
                 "<br/>" +
